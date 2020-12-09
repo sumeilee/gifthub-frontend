b0be8325fd11e2a591bdef231ca295e78988fc2d
Su Mei Lee <sumeilee@gmail.com>
2020-12-09 20:19:28 +0800

    setting loading spinner for offers and requests

--- src/components/pages/Requests.jsx
+++ src/components/pages/Requests.jsx
@@ -3,9 +3,7 @@ import { withRouter } from "react-router-dom";
 import { withCookies } from "react-cookie";
 import { Link } from "react-router-dom";
 import jwt from "jsonwebtoken";
-
-// import css later
-// import Item from "./Item";
+import Loader from "react-loader-spinner";
 
 import api from "../../services/api";
 
@@ -13,15 +11,14 @@ class Requests extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: true,
       list: [],
-      // me: {},
       user: null,
     };
   }
 
   componentDidMount() {
     const token = this.props.cookies.get("token");
-    // const me = jwt.decode(token);
     let user = null;
 
     if (token) {
@@ -31,8 +28,8 @@ class Requests extends React.Component {
 
     api.listRequests().then((response) => {
       this.setState({
+        loading: false,
         list: response.data,
-        // me,
         user: user,
       });
     });
@@ -56,113 +53,123 @@ class Requests extends React.Component {
 
   render() {
     return (
-      <div className="page-requests container mx-auto px-10 flex flex-col">
-        <br />
-        <table className="table-auto">
-          <tbody>
-            <tr className="bg-gray-200 h-10">
-              <td>
-                <div className="text-2xl font-bold text-center">REQUESTS</div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <br />
+      <>
+        {this.state.loading ? (
+          <div className="flex justify-center">
+            <Loader type="Hearts" color="#DB2777" height={80} width={80} />
+          </div>
+        ) : (
+          <div className="page-requests container mx-auto px-10 flex flex-col">
+            <br />
+            <table className="table-auto">
+              <tbody>
+                <tr className="bg-gray-200 h-10">
+                  <td>
+                    <div className="text-2xl font-bold text-center">
+                      REQUESTS
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <br />
 
-        <div>
-          {this.state.list.length > 0 ? (
-            this.state.list.map((element) => {
-              return (
-                <React.Fragment key={element._id}>
-                  <div className="container-item border-2 px-4 py-8 mx-4 my-4 rounded-lg border-green-500 border-opacity-75 shadow overflow-hidden">
-                    <div className="grid grid-cols-3 gap-4">
-                      <div className="col-span-2">
-                        <p className="text-base font-bold tracking-wider">
-                          {element.title}
-                        </p>
-                        <p>
-                          Date:{" "}
-                          {new Date(element.createdAt).toLocaleDateString(
-                            "en-GB",
-                            {
-                              year: "numeric",
-                              month: "long",
-                              day: "numeric",
-                            }
-                          )}
-                        </p>
-                        {/* <p>Item ID: {element._id}</p> */}
-                        {/* <p>Type: {element.postType}</p> */}
-                        <p className="truncate mr-2">
-                          Description: {element.description}
-                        </p>
-                        <p>Category: {element.category}</p>
-                        <p>
-                          Tags: {element.tags}
-                          <br />
-                        </p>
-                        <br />
-                        {/* // Toggle Item Owner Display */}
-                        {!this.state.user ||
-                        this.state.user.id !== element.postedBy._id ? (
-                          <p>
-                            Posted By:{" "}
-                            {`${element.postedBy.first_name} ${element.postedBy.last_name}`}
-                          </p>
-                        ) : (
-                          <p className="item-owner">
-                            Posted By: <b className="text-red-800">You</b>
-                          </p>
-                        )}
-                        {/* <p>
+            <div>
+              {this.state.list.length > 0 ? (
+                this.state.list.map((element) => {
+                  return (
+                    <React.Fragment key={element._id}>
+                      <div className="container-item border-2 px-4 py-8 mx-4 my-4 rounded-lg border-green-500 border-opacity-75 shadow overflow-hidden">
+                        <div className="grid grid-cols-3 gap-4">
+                          <div className="col-span-2">
+                            <p className="text-base font-bold tracking-wider">
+                              {element.title}
+                            </p>
+                            <p>
+                              Date:{" "}
+                              {new Date(element.createdAt).toLocaleDateString(
+                                "en-GB",
+                                {
+                                  year: "numeric",
+                                  month: "long",
+                                  day: "numeric",
+                                }
+                              )}
+                            </p>
+                            {/* <p>Item ID: {element._id}</p> */}
+                            {/* <p>Type: {element.postType}</p> */}
+                            <p className="truncate mr-2">
+                              Description: {element.description}
+                            </p>
+                            <p>Category: {element.category}</p>
+                            <p>
+                              Tags: {element.tags}
+                              <br />
+                            </p>
+                            <br />
+                            {/* // Toggle Item Owner Display */}
+                            {!this.state.user ||
+                            this.state.user.id !== element.postedBy._id ? (
+                              <p>
+                                Posted By:{" "}
+                                {`${element.postedBy.first_name} ${element.postedBy.last_name}`}
+                              </p>
+                            ) : (
+                              <p className="item-owner">
+                                Posted By: <b className="text-red-800">You</b>
+                              </p>
+                            )}
+                            {/* <p>
                           Posted By:{" "}
                           {`${element.postedBy.first_name} ${element.postedBy.last_name}`}
                         </p> */}
+                          </div>
+                          <div className="col2">
+                            <br />
+                            <p>Status: {element.status}</p>
+                            <p>Delivery: {element.delivery}</p>
+                            <br />
+                            <Link
+                              to={{
+                                pathname: `/items/${element._id}`,
+                                state: {
+                                  showDonate: true,
+                                },
+                              }}
+                              className="inline-flex justify-center text-gray-800 font-semibold px-4 py-2 rounded-md bg-yellow-300 hover:bg-yellow-400 hover:no-underline hover:text-gray-800"
+                            >
+                              View
+                            </Link>
+                            {/* // Toggle Chat Btn Display */}
+                            {!this.state.user ||
+                            this.state.user.id !== element.postedBy._id ? (
+                              <button
+                                type="submit"
+                                onClick={() =>
+                                  this.handleChatClick(
+                                    element.postedBy._id,
+                                    element._id
+                                  )
+                                }
+                                className="inline-flex justify-center text-gray-800 font-semibold mx-3 px-4 py-2 rounded-md bg-yellow-300 hover:bg-yellow-400 focus:outline-none"
+                              >
+                                Chat
+                              </button>
+                            ) : null}
+                          </div>
+                        </div>
                       </div>
-                      <div className="col2">
-                        <br />
-                        <p>Status: {element.status}</p>
-                        <p>Delivery: {element.delivery}</p>
-                        <br />
-                        <Link
-                          to={{
-                            pathname: `/items/${element._id}`,
-                            state: {
-                              showDonate: true,
-                            },
-                          }}
-                          className="inline-flex justify-center text-gray-800 font-semibold px-4 py-2 rounded-md bg-yellow-300 hover:bg-yellow-400 hover:no-underline hover:text-gray-800"
-                        >
-                          View
-                        </Link>
-                        {/* // Toggle Chat Btn Display */}
-                        {!this.state.user ||
-                        this.state.user.id !== element.postedBy._id ? (
-                          <button
-                            type="submit"
-                            onClick={() =>
-                              this.handleChatClick(
-                                element.postedBy._id,
-                                element._id
-                              )
-                            }
-                            className="inline-flex justify-center text-gray-800 font-semibold mx-3 px-4 py-2 rounded-md bg-yellow-300 hover:bg-yellow-400 focus:outline-none"
-                          >
-                            Chat
-                          </button>
-                        ) : null}
-                      </div>
-                    </div>
-                  </div>
-                </React.Fragment>
-              );
-            })
-          ) : (
-            <p>No requests found.</p>
-          )}
-        </div>
-        <br />
-      </div>
+                    </React.Fragment>
+                  );
+                })
+              ) : (
+                <p>No requests found.</p>
+              )}
+            </div>
+            <br />
+          </div>
+        )}
+      </>
     );
   }
 }
